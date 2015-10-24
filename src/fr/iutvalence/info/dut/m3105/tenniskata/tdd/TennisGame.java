cdd64bacc0de7a4b82af57e609efccfd8b865c48
Maxx <Maxx@Maxx-PC>
2015-10-24 13:55:37 +0200

    State at the end of the seance
    Pushed at home cause we forgot to push at school

--- src/fr/iutvalence/info/dut/m3105/tenniskata/tdd/TennisGame.java
+++ src/fr/iutvalence/info/dut/m3105/tenniskata/tdd/TennisGame.java
@@ -1,5 +1,50 @@
 package fr.iutvalence.info.dut.m3105.tenniskata.tdd;
 public class TennisGame
 {
+	public static final String FIFTEEN_ALL = "Fifteen - all";
+	public static final String LOVE_FIFTEEN = "Love - fifteen";
+	public static final String FIFTEEN_LOVE = "Fifteen - love";
+	public static final String LOVE_ALL = "Love - all";
+	
+	
+	private String score;
+	private int serverCount=0;
+	private int receiverCount=0;
+	private String serverScore;
+	private String receiverScore;
+	
+	public TennisGame()
+	{
+		this.score =  LOVE_ALL;
+	}
+	
+	public void serverScores()
+	{
+		serverCount += 1;
+	}
+	
+	public void receiverScores() 
+	{
+		receiverCount += 1;
+	}
+	
+	
+	public String getScore() 
+	{
+		this.calculScore();
+		return this.score;
+	}
+
+	private void calculScore() 
+	{
+//		switch (serverCount)
+//		{
+//		case 0:
+//			serverScore = "LOVE";
+//		}
+			//TODO : calculer tous les scores possibles	
+	}
+
+	
 
 }
--- test/fr/iutvalence/info/dut/m3105/tenniskata/tdd/TennisGameTest.java
+++ test/fr/iutvalence/info/dut/m3105/tenniskata/tdd/TennisGameTest.java
@@ -1,15 +1,54 @@
 package fr.iutvalence.info.dut.m3105.tenniskata.tdd;
 
 
+import static org.junit.Assert.*;
+
 import org.junit.Assert;
 import org.junit.Test;
 
 public class TennisGameTest
 {
+
 	@Test
-	public void aSimpleBeginningTest()
+	public void AnnonceScoreForNewGameShouldReturnNullForAllPlayers()
 	{
-		Assert.fail("...");
+		TennisGame tennisGame = new TennisGame();
+		assertEquals(tennisGame.getScore(), tennisGame.LOVE_ALL);
+	}
+	
+	@Test
+	public void AnnonceScoreWhenServerScoredShouldReturn_Fifteen_love()
+	{
+		TennisGame tennisGame = new TennisGame();
+		tennisGame.serverScores();
+		assertEquals(tennisGame.getScore(), tennisGame.FIFTEEN_LOVE);
+	}
+	
+	@Test
+	public void AnnonceScoreWhenReceiverScoredShouldReturn_Love_fifteen()
+	{
+		TennisGame tennisGame = new TennisGame();
+		tennisGame.receiverScores();
+		assertEquals(tennisGame.getScore(), tennisGame.LOVE_FIFTEEN);
 	}
 
+	@Test
+	public void AnnonceScoreWhenReceiverAndServerScoredShouldReturn_Fifteen_all()
+	{
+		TennisGame tennisGame = new TennisGame();
+		tennisGame.receiverScores();
+		tennisGame.serverScores();
+		assertEquals(tennisGame.getScore(), tennisGame.FIFTEEN_ALL);
+	}
+
+	@Test
+	public void AnnonceScoreWhateverWhoScoreAnyTimeShouldReturn_Fifteen_all()
+	{
+		TennisGame tennisGame = new TennisGame();
+		tennisGame.receiverScores();
+		tennisGame.serverScores();
+		tennisGame.serverScores();
+		tennisGame.serverScores();
+		assertEquals(tennisGame.getScore(), "Fifteen - forty");
+	}
 }
