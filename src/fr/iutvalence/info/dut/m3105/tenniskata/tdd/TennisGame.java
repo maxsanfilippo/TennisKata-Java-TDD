8c5c0de071cc6f0bdbc3c88c6eb9d8971f3c9b51
Maxx <Maxx@Maxx-PC>
2015-10-29 13:26:54 +0100

    Try to make it working
    not now

--- src/fr/iutvalence/info/dut/m3105/tenniskata/tdd/TennisGame.java
+++ src/fr/iutvalence/info/dut/m3105/tenniskata/tdd/TennisGame.java
@@ -3,20 +3,22 @@ package fr.iutvalence.info.dut.m3105.tenniskata.tdd;
 public class TennisGame
 {
 	public static final String FIFTEEN_ALL = "Fifteen - all";
-	public static final String LOVE_FIFTEEN = "Love - fifteen";
-	public static final String FIFTEEN_LOVE = "Fifteen - love";
+	public static final String LOVE_FIFTEEN = "Love - Fifteen";
+	public static final String FIFTEEN_LOVE = "Fifteen - Love";
 	public static final String LOVE_ALL = "Love - all";
 	
 	
 	private String score;
-	private int serverCount=0;
-	private int receiverCount=0;
+	private int serverCount;
+	private int receiverCount;
 	private String serverScore;
 	private String receiverScore;
 	
 	public TennisGame()
 	{
 		this.score =  LOVE_ALL;
+		serverCount = 0;
+		receiverCount = 0;
 	}
 	
 	public void serverScores()
@@ -41,15 +43,30 @@ public class TennisGame
 		switch (serverCount)
 		{
 		case 0:
-			serverScore = "LOVE";
+			serverScore = "Love";
 		case 1:
 			serverScore = "Fifteen";
 		case 2:
 			serverScore = "Thirteen";
 		case 3:
-			serverScore
+			serverScore = "Forty";		
 		}
-			//TODO : calculer tous les scores possibles	
+		
+		switch (receiverCount)
+		{
+		case 0:
+			receiverScore = "Love";
+		case 1:
+			receiverScore = "Fifteen";
+		case 2:
+			receiverScore = "Thirteen";
+		case 3:
+			receiverScore = "Forty";
+		}
+		
+		if (serverScore == receiverScore)
+			score = receiverScore + " - all";
+		score = serverScore + " - " + receiverScore;
 	}
 
 	
